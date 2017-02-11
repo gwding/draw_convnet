00748f21d6fe96efb15f4f12f5cfb9c2a81d2f67
Weiguang (Gavin) Ding <gwding@users.noreply.github.com>
2017-02-11 15:01:57 -0500

    Update draw_convnet.py

--- draw_convnet.py
+++ draw_convnet.py
@@ -69,29 +69,29 @@ def add_mapping(patches, colors, start_ratio, patch_size, ind_bgn,
 
     start_loc = top_left_list[ind_bgn] \
         + (num_show_list[ind_bgn] - 1) * np.array(loc_diff_list[ind_bgn]) \
-        + np.array([start_ratio[0] * size_list[ind_bgn],
+        + np.array([start_ratio[0] * 1,
                     -start_ratio[1] * size_list[ind_bgn]])
 
     end_loc = top_left_list[ind_bgn + 1] \
         + (num_show_list[ind_bgn + 1] - 1) \
         * np.array(loc_diff_list[ind_bgn + 1]) \
         + np.array([(start_ratio[0] + .5 * patch_size / size_list[ind_bgn]) *
-                    size_list[ind_bgn + 1],
+                    1,
                     -(start_ratio[1] - .5 * patch_size / size_list[ind_bgn]) *
                     size_list[ind_bgn + 1]])
 
-    patches.append(Rectangle(start_loc, patch_size, patch_size))
+    patches.append(Rectangle(start_loc, 1, patch_size))
     colors.append(Dark)
     patches.append(Line2D([start_loc[0], end_loc[0]],
                           [start_loc[1], end_loc[1]]))
     colors.append(Black)
-    patches.append(Line2D([start_loc[0] + patch_size, end_loc[0]],
+    patches.append(Line2D([start_loc[0] + 1, end_loc[0]],
                           [start_loc[1], end_loc[1]]))
     colors.append(Black)
     patches.append(Line2D([start_loc[0], end_loc[0]],
                           [start_loc[1] + patch_size, end_loc[1]]))
     colors.append(Black)
-    patches.append(Line2D([start_loc[0] + patch_size, end_loc[0]],
+    patches.append(Line2D([start_loc[0] + 1, end_loc[0]],
                           [start_loc[1] + patch_size, end_loc[1]]))
     colors.append(Black)
 
